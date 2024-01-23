48cfc7316b0d872d4f11a20deabd1e0f120e4b65
8bitbug <137716553+8bitbug@users.noreply.github.com>
2024-01-23 18:47:07 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 document.getElementById('button').onclick = function popup() {
     document.getElementById('button').style.display='none';
     document.getElementById('aboutMe').style.display='none';
-    document.getElementById('moreAboutMe').style.display='visible'
-    document.getElementById('popup').style.display='visible';
-    document.getElementById('languageKnow').style.display='visible';
-    document.getElementById('closepopup').style.display='visible';
+    document.getElementById('moreAboutMe').style.display='block'
+    document.getElementById('popup').style.display='block';
+    document.getElementById('languageKnow').style.display='block';
+    document.getElementById('closepopup').style.display='block';
 };
 
 //Closes the popup
 
 document.getElementById('closepopup').onclick = function closespopup() {
-    document.getElementById('button').style.display='visible';
-    document.getElementById('aboutMe').style.display='visible';
+    document.getElementById('button').style.display='block';
+    document.getElementById('aboutMe').style.display='block';
     document.getElementById('moreAboutMe').style.display='none'
     document.getElementById('popup').style.display='none';
     document.getElementById('languageKnow').style.display='none';
