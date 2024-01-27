25c47b2dc661a137691bf1daf8d46c2242d6c9a8
8bitbug <137716553+8bitbug@users.noreply.github.com>
2024-01-27 23:11:22 +0100

    Complete

--- index.js
+++ index.js
@@ -1,55 +1,44 @@
-//Allows me to manipulate html elemnts
-
-let button = document.getElementById('button');
-let aboutMe = document.getElementById('aboutme');
-let moreAboutMe = document.getElementById('moreAboutMe');
-let popup = document.getElementById('popup');
-let languageKnow = document.getElementById('languageKnow');
-let closepopup = document.getElementById('closepopup');
-let contactbutton = document.getElementById('contactbutton');
-let blogbutton = document.getElementById('blogbutton');
-let githubbutton = document.getElementById('githubbutton');
-let contactcontent = document.getElementById('contactbuttoncontent');
-
 //Opens the popup
 
-button.onclick = function openpopup() {
-    button.style.display='none';
-    aboutMe.style.display='none';
-    contactbutton.style.display='none';
-    blogbutton.style.display='none';
-    githubbutton.style.display='none';
-    moreAboutMe.style.display='block';
-    popup.style.display='block';
-    languageKnow.style.display='block';
-    closepopup.style.display='block';
+document.getElementById('button').onclick = function openpopup() {
+    document.getElementById('button').style.display='none';
+    document.getElementById('aboutMe').style.display='none';
+    document.getElementById('contactbutton').style.display='none';
+    document.getElementById('blogbutton').style.display='none';
+    document.getElementById('githubbutton').style.display='none';
+    document.getElementById('moreAboutMe').style.display='block';
+    document.getElementById('languageKnow').style.display='block';
+    document.getElementById('closepopup').style.display='block';
 };
 
 //Closes the popup
 
-closepopup.onclick = function closepopup() {
-    button.style.display='block';
-    aboutMe.style.display='block';
-    moreAboutMe.style.display='none'
-    popup.style.display='none';
-    languageKnow.style.display='none';
-    closepopup.style.display='none';
-    contactbutton.style.display='flex';
-    blogbutton.style.display='flex';
-    githubbutton.style.display='flex';
-    contactcontent.style.display='none';
+document.getElementById('closepopup').onclick = function closepopup() {
+    document.getElementById('button').style.display='block';
+    document.getElementById('aboutMe').style.display='block';
+    document.getElementById('contactbutton').style.display='flex';
+    document.getElementById('blogbutton').style.display='flex';
+    document.getElementById('githubbutton').style.display='flex';
+    document.getElementById('moreAboutMe').style.display='none';
+    document.getElementById('languageKnow').style.display='none';
+    document.getElementById('closepopup').style.display='none';
+    document.getElementById('contactbuttoncontent').style.display='block';
 };
 
-contactbutton.onclick = function openContactcontent() {
-    button.style.display='none';
-    aboutMe.style.display='none';
-    closepopup.style.display='block';
-    contactbutton.style.display='none';
-    blogbutton.style.display='none';
-    githubbutton.style.display='none';
-    contactcontent.style.display='block';
+//opens popup
+
+document.getElementById('contactbutton').onclick = function openContactcontent() {
+    document.getElementById('button').style.display='none';
+    document.getElementById('aboutMe').style.display='none';
+    document.getElementById('contactbutton').style.display='none';
+    document.getElementById('blogbutton').style.display='none';
+    document.getElementById('githubbutton').style.display='none';
+    document.getElementById('closepopup').style.display='block';
+    document.getElementById('contactbuttoncontent').style.display='block';
 };
 
-githubbutton.onclick = function opengithub() {
+//Makes div open a website in a new tab
+
+document.getElementById('githubbutton').onclick = function opengithub() {
     window.open('https://github.com/8bitbug', '_blank');
 };
