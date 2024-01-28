9737e466032572d76a262176e4521921b1e5117f
8bitbug <137716553+8bitbug@users.noreply.github.com>
2024-01-28 16:49:59 +0000

    Optimising for mobile?

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 let githubbutton = document.getElementById('githubbutton');
 
+const width = screen.width;
+const height = screen.height;
+const widthMax = 420;
+const heightMax = 900;
+
 githubbutton.onclick = function() {
     window.open('https://github.com/8bitbug', '_blank')
-}
+};
+
+if (width < widthMax && height < heightMax) {
+    document.getElementById('navigation').style.width='360px';
+    document.getElementById('navbuttons').style.right='10px';
+};
